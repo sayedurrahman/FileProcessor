c49bd6670e4f43497c3b71c2e9d12d7fb17404a9
sayedurrahman <sayedur.rahmans@gmail.com>
2020-09-29 10:11:33 +0600

    Service added

---
+++ FileProcessor.Service/FileProcessorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProcessor.Service
+{
+    public class FileProcessorController
+    {
+        public string RepositoryPath { get; set; }
+        public FileProcessorController(string repositoryPath)
+        {
+            RepositoryPath = repositoryPath;
+        }
+        
+        public void Process(){
+            /*
+             * Open repo
+             * Get list of file
+             * foreach file
+             *      get the instance of IFile
+             *      instance.IFileLoader.Load()
+             *      instance.IFIleProcesser.Process()
+             *      instance.IFileSaver.Save()
+             * */
+
+        }
+
+
+    }
+}
