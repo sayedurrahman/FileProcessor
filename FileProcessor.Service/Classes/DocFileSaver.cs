30a8341277ecbfcfea7260f68204b156c2df6358
sayedurrahman <sayedur.rahmans@gmail.com>
2020-09-30 03:14:12 +0600

    Memory leak prevented

--- FileProcessor.Service/Classes/DocFileSaver.cs
+++ FileProcessor.Service/Classes/DocFileSaver.cs
@@ -14,6 +14,7 @@ namespace FileProcessor.Service.Classes
         {
             Document doc = (Document)document;
             doc.SaveToFile(fileName, FileFormat.Docx);
+            doc.Dispose();
         }
     }
 }
