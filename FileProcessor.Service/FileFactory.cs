64a747a3e60eaa93c95ebe96065c17aa19b0ec83
sayedurrahman <sayedur.rahmans@gmail.com>
2020-09-29 15:43:20 +0600

    Create FileFactory.cs

---
+++ FileProcessor.Service/FileFactory.cs
@@ -0,0 +1,26 @@
+﻿using FileProcessor.Service.Classes;
+using FileProcessor.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProcessor.Service
+{
+    public static class FileFactory
+    {
+        public static IFile GetInstance(FileInfo fileName){
+            switch (fileName.Extension.ToLower())
+            {
+                case "doc":
+                case "docx":
+                    return new DocFile();
+                default:
+                    // This file type not supported, so continue for next file
+                    return null;
+            }
+        }
+    }
+}
