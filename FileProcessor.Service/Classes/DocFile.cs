ce6b12afc0d3f87b5a4b2a1ce68991b3cec1f62c
sayedurrahman <sayedur.rahmans@gmail.com>
2020-09-29 15:43:02 +0600

    base classes added

---
+++ FileProcessor.Service/Classes/DocFile.cs
@@ -0,0 +1,20 @@
+﻿using FileProcessor.Service.Interfaces;
+using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProcessor.Service.Classes
+{
+    public class DocFile : CompanyFile
+    {
+        public DocFile()
+        {
+            Loader = new DocFileLoader();
+            Processer = new DocFileProcessor();
+            Saver = new DocFileSaver();
+        }        
+    }
+}
